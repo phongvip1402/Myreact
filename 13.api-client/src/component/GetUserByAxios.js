fcd3f7f260fcf985712bd00c216c818f11c514a2
Thanh Phong <hoangtu9x1402@gmail.com>
2022-12-02 18:32:43 +0700

    Get user by axios

---
+++ 13.api-client/src/component/GetUserByAxios.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+import { axios } from "axios";
+
+export default function GetUserByAxios() {
+  const [users, setUsers] = useState();
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/api/users")
+      .then((res) => {
+       setUsers({ users: res.data });
+      })
+      .catch((err) => {
+        throw err;
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>Users</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}> {user.name} </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
