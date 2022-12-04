3b402feda6362646bee5856922ccc663c672cec1
Thanh Phong <hoangtu9x1402@gmail.com>
2022-12-05 02:39:13 +0700

    upadate Loading by axios promise

--- 13.api-client/src/component/LoadingByAxios.js
+++ 13.api-client/src/component/LoadingByAxios.js
@@ -7,20 +7,32 @@ function LoadingByAxios() {
 
   useEffect(() => {
     setLoading(true);
-    setTimeout(() => {
-      axios
-        .get("http://localhost:3001/user")
-        .then((res) => {
-          setUser(res.data); 
-        })
-        .catch((err) => {
-          throw err;
-        })
-        .finally(() => {
-          setLoading(false);
-        });
-    }, 3000);
+    getUsers()
+      .then((res) => {
+        setUser(res.data);
+      })
+      .catch((err) => {
+        throw err;
+      })
+      .finally(() => {
+        setLoading(false);
+      });
   }, []);
+
+  const getUsers = () => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        axios
+          .get("http://localhost:3001/user")
+          .then((res) => {
+            resolve(res);
+          })
+          .catch((err) => {
+            reject(err);
+          });
+      }, 3000);
+    });
+  };
 
   if (loading) return <p>loading...</p>;
   return (
